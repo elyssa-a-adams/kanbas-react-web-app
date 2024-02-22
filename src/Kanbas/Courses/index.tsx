c207d543ba97f81bb8814e49cd871049611c3d26
elyssa-a-adams <elyssaa1995@gmail.com>
2024-02-22 18:36:43 -0500

    fix: assignments

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,6 +6,7 @@ import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import "./index.css";
+import Assignments from "./Assignments";
 
 
 function Courses() {
@@ -23,7 +24,7 @@ function Courses() {
             <Route path="/" element={<Navigate to="Home" />} />
             <Route path="Home" element={<Home/>} />
             <Route path="Modules" element={<Modules/>} />
-            <Route path="Assignments" element={<h1>Assignments</h1>} />
+            <Route path="Assignments" element={<Assignments />} />
             <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>}/>
             <Route path="Grades" element={<h1>Grades</h1>} />
           </Routes>
